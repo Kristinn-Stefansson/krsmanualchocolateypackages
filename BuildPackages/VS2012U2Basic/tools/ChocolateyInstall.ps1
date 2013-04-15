9ae47c0150737d2b15c2bb86672e0214481501de
Kristinn Stefánsson <krs@ru.is>
2013-04-15 19:02:46 +0000

    Messing around with VS2012 BoxStarter package

---
+++ BuildPackages/VS2012U2Basic/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+try {
+Update-ExecutionPolicy Unrestricted
+cinstm pswindowsupdate # Alternative version of windows updater PS interface
+Import-Module PsWindowsUpdate
+# Install-WindowsUpdate -AcceptEula
+if(Get-WURebootStatus -Silent){Invoke-Reboot}
+cwebpi DACFX
+cinstm tfs2012powertools
+cwebpi SSDTVS2010
+cwebpi SSDTVS2012
+cinstm -source \\fisnets2\systems$\tfs\pub\SharedFunctionality\SharedBinaries\Chocolatey VS2012.SSDTBI 
+cinstm -source \\fisnets2\systems$\tfs\pub\SharedFunctionality\SharedBinaries\Chocolatey VS2012.Update2
+Get-WUInstall -AcceptAll -IgnoreRebootRequired
+if(Get-WURebootStatus -Silent){Invoke-Reboot}
+    Write-ChocolateySuccess 'DevSetup2013'
+} catch {
+  Write-ChocolateyFailure 'DevSetup2013' $($_.Exception.Message)
+  throw
+}
