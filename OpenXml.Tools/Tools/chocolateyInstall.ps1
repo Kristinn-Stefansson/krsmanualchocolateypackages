352c0a575c66553372e9b23b4504b53e1907ba35
Kristinn Stefánsson <krs@ru.is>
2013-06-20 17:23:00 +0000

    Added OpenXML

---
+++ OpenXml.Tools/Tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'OpenXml.Tools'
+$installerType = 'MSI' 
+$url = 'http://download.microsoft.com/download/5/5/3/553C731E-9333-40FB-ADE3-E02DC9643B31/OpenXMLSDKToolV25.msi' # download url
+$url64 = $url # 64bit URL uses the same as $url
+$silentArgs = '/quiet /norestart' # 
+$validExitCodes = @(0,3010) 
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ gather.ps1
@@ -0,0 +1,32 @@
+
+cd .\VS2012.Ultimate
+cpack
+cd ..\VS2012.NET45
+cpack
+cd ..\VS2012.Update1
+cpack
+cd ..\VS2012.Update2
+cpack
+cd ..\VS2012.SSDTBI
+cpack
+cd ..\VS2012.SSDT11
+cpack
+cd ..\VS2010.SSDT10
+cpack
+cd ..\OpenXml.SDK
+cpack
+cd ..\OpenXml.Tools
+cpack
+cd ..\developer.setup.extension 
+cpack
+cd ..\BuildPackages\VS2012U2Basic
+cpack
+cd ..\..\
+
+$g = Get-ChildItem -Recurse -Include *.nupkg 
+$g | % {
+    Copy-Item $_.FullName -destination C:\NoBackup\SkyDrive\Code\Chocolatey -Force
+}
+ $g | % {
+    Move-Item $_.FullName -destination \\fisnets2\systems$\TFS\Pub\SharedFunctionality\SharedBinaries\Chocolatey -Force
+}
